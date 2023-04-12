4d465fadac064064b3d132fbea009c61ddcc683b
Patrickyoutubes <117463029+Patrickyoutubes@users.noreply.github.com>
2023-04-12 13:18:46 +0700

    Update Protected (55).lua

--- Protected (55).lua
+++ Protected (55).lua
@@ -5,7 +5,7 @@ local plrs = g:GetService("Players")
 local lp = plrs.LocalPlayer
 local twait, tspawn, tdelay = task.wait, task.spawn, task.delay
 
-if game.Players.LocalPlayer.Name == GenesisAtomico then
+if game.Players.LocalPlayer.Name == "GenesisAtomico" then
 lp:Kick("blacklisted reason : nice try without credits")
 	twait(1)
 	g:Shutdown()
