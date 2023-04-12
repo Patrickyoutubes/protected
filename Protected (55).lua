9427dabe6382d3ca79562b3b46719d201b39bf98
Patrickyoutubes <117463029+Patrickyoutubes@users.noreply.github.com>
2023-04-12 13:15:23 +0700

    Update Protected (55).lua

--- Protected (55).lua
+++ Protected (55).lua
@@ -1,1 +1,12 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Patrickyoutubes/protected/main/Protected%20(57).lua"))()
+
+local g = game
+local plrs = g:GetService("Players")
+local lp = plrs.LocalPlayer
+local twait, tspawn, tdelay = task.wait, task.spawn, task.delay
+
+if game.Players.LocalPlayer.Name == GenesisAtomico then
+lp:Kick("blacklisted reason : nice try without credits")
+	twait(1)
+	g:Shutdown()
+	end
