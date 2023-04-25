17e248d21dd4f73139a193698c457c3ffe31ed3a
Patrickyoutubes <117463029+Patrickyoutubes@users.noreply.github.com>
2023-04-25 13:45:13 +0700

    Update Protected (22).lua

--- Protected (22).lua
+++ Protected (22).lua
@@ -1,1 +1,8 @@
+
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "";
+	Text = "Wait";
+	Icon = ""})
+Duration = 16;
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Patrickyoutubes/protected/main/omfuscate.lua"))()
