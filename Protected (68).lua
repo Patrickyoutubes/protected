1cbcfbb807a42a5be495fca4c6a4e81bc09cc95d
Patrickyoutubes <117463029+Patrickyoutubes@users.noreply.github.com>
2023-05-06 13:15:51 +0700

    Update Protected (68).lua

--- Protected (68).lua
+++ Protected (68).lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Patrickyoutubes/protected/main/Protected%20(1)%20(5).lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Patrickyoutubes/protected/main/Protected%20(71).lua"))()
